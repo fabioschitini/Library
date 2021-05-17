68c35856d4ed1c4d129a1ca5bb7d49ea612856e3
Fabio Schitini <fabioschitini1@hotmail.com>
2021-05-17 19:12:38 -0300

    fixing main bug nad cleaning the code

--- script.js
+++ script.js
@@ -1,113 +1,123 @@
-window.addEventListener('load', (event) => {
-    lookForArray()
-  });
   let myLibrary1 = [];
-  console.log(myLibrary1)
+  window.addEventListener('load', loadSaved);
   
-class Library{
+const Library=(title,author,pages)=>{
     
-
-    constructor(title,author,pages){
-        this.title=title
-    this.author=author
-    this.pages=pages
-    }
-    
-     addBookToLibrary() {
-        console.log(myLibrary)
-
-        let title= prompt("What book would you like to find?")
-       let author= prompt("Who is the author of the book?")
-     let pages= prompt("How many pages does the book have?")
-      myLibrary1.push(new Library(title,author,pages))
-     }
+     return{title,
+        author,
+        pages,
+        }
 }
-let container=document.querySelector("#books");
+
+function addBookToLibrary() {
+    let title= prompt("What book would you like to find?")
+    let author= prompt("Who is the author of the book?")
+   let pages= prompt("How many pages does the book have?")
+   myLibrary1.push( Library(title,author,pages))
+}
+
+function clickButton(){
 let btn= document.querySelector("#new")
 btn.addEventListener("click",bookDisplay)
 let btnn=document.querySelectorAll("#btn")
 let btnn2=document.querySelectorAll("#btn2")
+btn.addEventListener("click",saveArray)
+}
+
+clickButton()
    
 
 
 function bookDisplay(){
-    console.log(myLibrary1)
-
-    let add= new Library
-    add.addBookToLibrary()
-    
-   
-
-    myLibrary1.slice(myLibrary1.length-1).forEach(books=>setOnLibrary(books) )
-
+   addBookToLibrary()
+    myLibrary1.slice(myLibrary1.length-1).forEach(books=>{
+        createBooks.DomInsert(books)
+        toogleStatus(books)
+        removeBooks(books)   
+    } )
 }
 
 function saveArray(){
     let myLibrary_serialized=JSON.stringify(myLibrary1)
-    localStorage.setItem("myLibrary1",myLibrary_serialized)
-    
-    
-}
+        localStorage.setItem("myLibrary1",myLibrary_serialized)
+        }
+
 function lookForArray(){
     let myLibrary_deserialized=JSON.parse(localStorage.getItem("myLibrary1"))
-myLibrary=myLibrary_deserialized
+        myLibrary1=myLibrary_deserialized
 }
+function loadSaved(){
+    lookForArray()
+    console.log(myLibrary1)
+    myLibrary1.forEach(books=>{
+        createBooks.DomInsert(books)
+        toogleStatus(books)
+        removeBooks(books)   
+    })
+}
+const createBooks=setOnLibrary()
 
-btn.addEventListener("click",saveArray)
-
-
-window.onload = () => {
-
-    myLibrary1.forEach(books=>setOnLibrary(books) )
-};
-
-
-  function setOnLibrary(books){
-    
+  function setOnLibrary(){
+      
+      function DomInsert(books){
+        let container=document.querySelector("#books");
         let butto=document.createElement("button") 
-        let book=document.createElement("h1") 
-        let butto2=document.createElement("button") 
-    book.textContent=books.title
-    book.id=myLibrary1.indexOf(books)
-    butto.id="btn"
-    butto2.id="btn2"
-
-    butto.textContent="Delete"
-    butto2.textContent="Toogle"
-
-    let information=document.createElement("p") 
-    let information2=document.createElement("p")
-    let information3=document.createElement("p") 
-    information.textContent=`Author:${books.author}`
-    information2.textContent=`Pages:${books.pages}`
-    information3.textContent=`Status:Did not read it yet.`
-
-    container.appendChild(book)
-    book.appendChild(butto)
-    book.appendChild(butto2)
-    book.appendChild(information)
-    book.appendChild(information2)
-    book.appendChild(information3)
-
-     btnn=document.querySelectorAll("#btn")
-     btnn.forEach(function (btnn){ btnn.addEventListener("click",function(){
-        let remo=document.getElementById(myLibrary1.indexOf(books).toString())
-        remo.remove()
-    })})
-
-    btnn2=document.querySelectorAll("#btn2")
+       let book=document.createElement("h1") 
+       let butto2=document.createElement("button") 
+           book.textContent=books.title
+           book.id=myLibrary1.indexOf(books)
+           butto.id="btn"
+           butto2.id="btn2"
+   
+           butto.textContent="Delete"
+           butto2.textContent="Toogle"
+   
+       let information=document.createElement("p") 
+       let information2=document.createElement("p")
+       let information3=document.createElement("p") 
+       information3.id=-myLibrary1.indexOf(books)-1
+       console.log(information3.id)
+           information.textContent=`Author:${books.author}`
+           information2.textContent=`Pages:${books.pages}`
+           information3.textContent=`Status:Did not read it yet.`
+   
+           container.appendChild(book)
+           book.appendChild(butto)
+           book.appendChild(butto2)
+           book.appendChild(information)
+           book.appendChild(information2)
+           book.appendChild(information3)
+      }
+      
+return{
+    DomInsert,
+}
+    }
+    function removeBooks(books){
+        btnn=document.querySelectorAll("#btn")
+       btnn.forEach(function (btnn){ btnn.addEventListener("click",function(){
+       let remo=document.getElementById(myLibrary1.indexOf(books).toString())
+       myLibrary1.splice(myLibrary1.indexOf(books),1)
+       saveArray()
+       remo.remove()
+   })})
+}
+    function toogleStatus(books){
+        let information3=document.getElementById(`${-myLibrary1.indexOf(books)-1}`)
+        console.log(information3)
+        let btnn2=document.querySelectorAll("#btn2")
     btnn2.forEach(function (btnn2){ btnn2.addEventListener("click",function(){
        if(information3.textContent===`Status:Did not read it yet.`){
+           console.log("if")
            information3.textContent=`Status:Already read it!`
-           book.appendChild(information3)
-
        }
        else if(information3.textContent===`Status:Already read it!`){
-        information3.textContent=`Status:Did not read it yet.`
-           book.appendChild(information3)
+        console.log("else if")
 
+        information3.textContent=`Status:Did not read it yet.`
        }
    })})
+}
 
-    }
+    
   
